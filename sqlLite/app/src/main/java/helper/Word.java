7023600fbb940b658e8e3d73415b3334effe351d
Softwarica College Lab <softwarica@softwarica.com>
2019-05-02 11:51:37 +0545

    slqlite

--- sqlLite/app/src/main/java/helper/Word.java
+++ sqlLite/app/src/main/java/helper/Word.java
@@ -1,4 +1,38 @@
 package helper;
 
-public class Word {
+public class Word
+{
+    private int wordID;
+    private String word;
+    private String meaning;
+
+    public int getWordID() {
+        return wordID;
+    }
+
+    public void setWordID(int wordID) {
+        this.wordID = wordID;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+
+    public Word(int wordID, String word, String meaning) {
+        this.wordID = wordID;
+        this.word = word;
+        this.meaning = meaning;
+    }
 }
